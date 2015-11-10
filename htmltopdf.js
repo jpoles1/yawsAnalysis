eb6fbc64a0c557c523e1200f206787d593fef370
Jordan Poles <jpoles1@gmail.com>
2015-11-10 14:53:10 -0600

    Added some content.

--- htmltopdf.js
+++ htmltopdf.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./index.html', 'utf8');
-var options = {"height": "36in", "width": "36in"};
+var options = {"height": "48in", "width": "36in"};
 
 pdf.create(html, options).toFile('./index.pdf', function(err, res) {
     if (err) return console.log(err);
